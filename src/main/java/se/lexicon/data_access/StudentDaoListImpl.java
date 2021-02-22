994eb5cc8ad37493ef0507b840a1f0047d70fad8
Armin <fajkan10@gmail.com>
2021-02-22 11:19:32 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/data_access/StudentDaoListImpl.java
@@ -0,0 +1,10 @@
+package se.lexicon.data_access;
+
+import se.lexicon.models.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDaoListImpl {
+    List<Student> students = new ArrayList<Student>();
+}
