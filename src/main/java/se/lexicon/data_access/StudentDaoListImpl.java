c995b030336534dfaf74eaed96cd7059e0085c1c
Armin <fajkan10@gmail.com>
2021-02-22 11:50:42 +0100

    done

--- src/main/java/se/lexicon/data_access/StudentDaoListImpl.java
+++ src/main/java/se/lexicon/data_access/StudentDaoListImpl.java
@@ -5,6 +5,30 @@ import se.lexicon.models.Student;
 import java.util.ArrayList;
 import java.util.List;
 
-public class StudentDaoListImpl {
+public class StudentDaoListImpl implements StudentDao{
     List<Student> students = new ArrayList<Student>();
+
+    @Override
+    public Student save(Student student) {
+        Student original = find(student.getId());
+        original.setId(student.getId());
+        original.setName(student.getName());
+        return student;
+    }
+
+    @Override
+    public Student find(int id) {
+
+        return students.stream().filter(Student -> Student.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return students;
+    }
+
+    @Override
+    public void delete(int id) {
+        students.removeIf(student -> student.getId()==id);
+    }
 }
